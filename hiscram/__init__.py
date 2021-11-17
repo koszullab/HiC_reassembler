090ff7bb9c3d8c6f5d997801c863a89fdba840a2
cmdoret <cyril.mattheydoret@gmail.com>
2021-11-17 13:27:53 +0100

    improve package metadata and fix dependencies

--- hiscram/__init__.py
+++ hiscram/__init__.py
@@ -1,6 +1,9 @@
 import pathlib
 
 __version__ = "0.0.1"
+__author__ = "Cyril Matthey-Doret, Jules Olayé, Amaury Bignaud"
+__copyright__ = "Copyright © 2017-2018, Institut Pasteur, Paris, France"
+__LICENSE__ = "MIT"
 SCRAMBLE_CONFIG_PATH = str(
     pathlib.Path(__file__).parent / "scramble" / "config" / "template.json"
 )
--- setup.py
+++ setup.py
@@ -1,16 +1,47 @@
+#!/usr/bin/env python3
+"""Structural variants simulation, detection and ressembly using Hi-C contacts.
+"""
+
 import re
+import codecs
 from setuptools import setup, find_packages
 
 with open("hiscram/__init__.py", "r") as init:
     init_conts = init.read()
     vers_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    version = re.search(vers_regex, init_conts, re.MULTILINE)
-    if not version:
+    VERSION = re.search(vers_regex, init_conts, re.MULTILINE)
+    if not VERSION:
         raise RuntimeError(f"Cannot find version string in __init__.py")
+
+DESCRIPTION = __doc__.strip("\n")
+
+with codecs.open("README.md", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+with open("requirements.txt", "r") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Operating System :: OS Independent",
+]
+
 
 setup(
     name="HiC_reassembler",
-    version=version,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
     packages=find_packages(),
     entry_points={"console_scripts": ["hiscram=hiscram.cli:entry_point"]},
+    install_requires=REQUIREMENTS,
 )
